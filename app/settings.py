594e2ea7ea69fb3891cd8dd1c27cc87be74bacf1
shoko <shoko.in.kenya@gmail.com>
2022-02-26 02:11:12 +0900

    画像保存エラー

--- app/settings.py
+++ app/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = (
 )
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Password validation
@@ -219,7 +219,7 @@ else:
 
 if not DEBUG:
     STATIC_ROOT='/usr/share/nginx/html/static'
-    MEDIA_ROOT='/usr/share/nginx/html/media'
+    MEDIA_ROOT='/usr/share/nginx/html/media/'
 else:
     STATIC_ROOT = '/static'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
