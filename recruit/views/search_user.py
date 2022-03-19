c4bb851792669431dcf348c7fee2d24962861eea
shoko <shoko.in.kenya@gmail.com>
2022-03-19 16:18:56 +0900

    求職者検索画面、会社マイページ一部

--- recruit/views/search_user.py
+++ recruit/views/search_user.py
@@ -6,6 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from ..models import ItemModel, UserFavoriteItemModel
 from ..forms.item import ItemSearchForm
 from users.models import NormalUser
+from app.const import CHOICES
 
 
 app_name = "recruit"
@@ -15,9 +16,20 @@ class SearchUserView(TemplateView):
 
     def get(self, request):
         normal_users = NormalUser.objects.all()
+        user_cnt = len(normal_users)
+        for normal_user in normal_users:
+            normal_user.status = CHOICES.STATUS[normal_user.status][1]
+            normal_user.gender = CHOICES.GENDER[normal_user.gender][1]
+            normal_user.education_status = CHOICES.EDUCATION_STATUS[normal_user.education_status][1]
+            normal_user.school_type = CHOICES.SCHOOL_TYPE[normal_user.school_type][1]
+
+        
+
+
     
         return  self.render_to_response({
             "normal_users":normal_users,
+            "user_cnt":user_cnt,
         })  
      
     # def get(self, request):
