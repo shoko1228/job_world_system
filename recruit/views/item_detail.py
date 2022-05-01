9761dfb8c52bdd3b0fa1d7d1867e5fba87934b99
shoko <shoko.in.kenya@gmail.com>
2022-05-01 18:01:22 +0900

    【求人詳細】いいね機能

--- recruit/views/item_detail.py
+++ recruit/views/item_detail.py
@@ -1,9 +1,12 @@
+from asyncio import FastChildWatcher
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.views.generic import TemplateView,DetailView
 
 from ..models import ItemModel, UserFavoriteItemModel
 from ..forms.item import ItemSearchForm
+from app.const import USER_TYPE
+from django.http import Http404
 
 
 app_name = "recruit"
@@ -18,8 +21,18 @@ class ItemDetailView(TemplateView):
     template_name = f"{app_name}/item_detail.html"
     
     def get(self, request):
-        item = ItemModel.objects.filter(id=request.GET.get("id")).first()
+        item = ItemModel.objects.filter(id=request.GET.get("id")).first()    
+          
         #todo if そのIDのitem無いときなど、他のページに戻す処理
+        if not item:
+            return Http404("このページは表示できません")
+        item.is_favorited =False 
+        if self.request.user.is_authenticated:
+            if request.user.user_type == USER_TYPE.NORMAL_USER:
+                is_favorite = UserFavoriteItemModel.objects.filter(normal_user=request.user.normal_user, item=item).all()
+                if is_favorite:
+                    item.is_favorited = True
+
 
 
         return self.render_to_response({
